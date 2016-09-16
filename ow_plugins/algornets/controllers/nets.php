572d0ff6782cace32bb79fc736d9a68cfa50e0c3
algor1 <algor1@users.noreply.github.com>
2016-09-16 10:56:40 +0300

    Update nets.php

--- ow_plugins/algornets/controllers/nets.php
+++ ow_plugins/algornets/controllers/nets.php
@@ -1,3 +1,8 @@
 class ALGORNETS_CTRL_Nets extends OW_ActionController 
 { 
+ public function index() 
+    { 
+        $this->setPageTitle("Nets"); 
+        $this->setPageHeading("Nets"); 
+    } 
 }
